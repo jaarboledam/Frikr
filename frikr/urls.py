5c1b3da6d01cfc0d3ec4abe62bce9752a0750276
jaarboledam <jaarboledam@gmail.com>
2016-09-11 23:47:24 -0500

    Se agrega el login y logout además de un menú de navegación

--- frikr/urls.py
+++ frikr/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from photos.views import home, photo_detail
+from users.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', login),
+    url(r'^logout/$', logout),
     url(r'^$', home),
     url(r'^photos/(?P<pk>\d+)$', photo_detail),  # ?P<nombre_parametro>
 ]
