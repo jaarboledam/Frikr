2f8007a58e2cd3b958e68114a9134161acd150dc
jaarboledam <jaarboledam@gmail.com>
2016-09-13 17:00:14 -0500

    Se crea el primer endpoint del API con django-rest-framework

---
+++ users/api.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.views import View
+from rest_framework.renderers import JSONRenderer
+
+from users.serializers import UserSerializer
+
+
+class UserListAPI(View):
+    """
+    Endpoint de listado de usuarios
+    """
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        renderer = JSONRenderer()
+        data = renderer.render(serializer.data)
+        return HttpResponse(data)
