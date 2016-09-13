7c9ee4bc9773c44c0ce567ccf2abe0e847efd223
jaarboledam <jaarboledam@gmail.com>
2016-09-13 17:34:05 -0500

    Se crea el endpoint de creacion de un usuario

--- users/api.py
+++ users/api.py
@@ -1,18 +1,25 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views import View
-from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
 from users.serializers import UserSerializer
 
 
-class UserListAPI(View):
+class UserListAPI(APIView):
     """
     Endpoint de listado de usuarios
     """
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-        renderer = JSONRenderer()
-        data = renderer.render(serializer.data)
-        return HttpResponse(data)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
